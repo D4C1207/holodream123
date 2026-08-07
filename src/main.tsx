7d82307d96817653050c16fecabf49dba7fd20ea
D4C1207 <s1113708@mail.yzu.edu.tw>
2026-08-08 03:06:41 +0800

    Load team favorites styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { LocaleProvider } from "./i18n/LocaleContext";
 import "./styles.css";
 import "./d4c.css";
+import "./favorites.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
